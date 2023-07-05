1ba95bcacf4c96fb95fa2d7e204036a390a4739b
Alex-Viet <alex.night.power@gmail.com>
2023-07-05 20:39:30 +0300

    Настроено форматирование кода через Prettier

--- js/index.js
+++ js/index.js
@@ -1,20 +1,18 @@
 let gameComps = {
-    difficultyLevel: "",
-}
+    difficultyLevel: '',
+};
 
-const easyMode = document.getElementById("dif-easy");
-const mediumMode = document.getElementById("dif-med");
-const hardMode = document.getElementById("dif-hard");
-const goButton = document.getElementById("button-go");
-const difLevelBtnElems = document.querySelectorAll("input");
-
+const goButton = document.getElementById('button-go');
+const difLevelBtnElems = document.querySelectorAll('input');
 
 for (const difLevelBtnElem of difLevelBtnElems) {
-    difLevelBtnElem.addEventListener(("click"), () => {
+    difLevelBtnElem.addEventListener('click', () => {
         gameComps.difficultyLevel = difLevelBtnElem.value;
 
-        goButton.addEventListener(("click"), () => {
-            alert(`Далее будет запускаться игра в зависимости от уровня сложности: ${gameComps.difficultyLevel}`);
-        })
-    })
+        goButton.addEventListener('click', () => {
+            alert(
+                `Далее будет запускаться игра в зависимости от уровня сложности: ${gameComps.difficultyLevel}`,
+            );
+        });
+    });
 }
