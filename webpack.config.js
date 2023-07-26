feb4c7efdcd23692d0f8402d8868c11b0c760c51
Alex-Viet <alex.night.power@gmail.com>
2023-07-26 14:59:05 +0300

    Проект переведен на Typescript

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
-  entry: './js/index.js',
+  entry: './js/index.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
@@ -14,6 +14,11 @@ module.exports = {
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   module: {
     rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
       { test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader'] },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
@@ -24,6 +29,9 @@ module.exports = {
         type: 'asset/resource',
       },
     ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
   },
   optimization: {
     minimizer: ['...', new CssMinimizerPlugin()],
